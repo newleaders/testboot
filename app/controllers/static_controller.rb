7fae965a99e326d415996267a238f2ca5355cc83
Kenny Johnston <kjohnston@newleaders.com>
2012-10-09 16:24:54 -0700

    Add sitemap to provide index of sample views.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,14 @@
 class StaticController < ApplicationController
 
+  def sitemap
+    render "sitemap", layout: "sitemap"
+  end
+
   def show
-  	@paginated_items = Kaminari.paginate_array((1..200).to_a).page(params[:page]).per(20)
+    if params[:path] == "index"
+  	  @paginated_items = Kaminari.paginate_array((1..200).to_a).page(params[:page]).per(20)
+    end
+    render template: "static/#{params[:path]}"
   end
 
 end
