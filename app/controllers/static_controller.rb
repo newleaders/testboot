173d37655c1e8fc9b454ba6d983ae9822268268d
Kenny Johnston <kjohnston@newleaders.com>
2012-10-10 00:35:26 -0700

    Create show view with metrics and tabbed panels.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,9 +5,7 @@ class StaticController < ApplicationController
   end
 
   def show
-    if params[:path] =~ /index/
-  	  @paginated_items = Kaminari.paginate_array((1..200).to_a).page(params[:page]).per(20)
-    end
+ 	  @paginated_items = Kaminari.paginate_array((1..200).to_a).page(params[:page]).per(20)
     render template: "static/#{params[:path]}"
   end
 
