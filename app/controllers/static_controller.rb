a4ee64b59d0ca5e4f7dc5d4ebac3a253a2d2214d
Kenny Johnston <kjohnston@newleaders.com>
2012-10-09 21:59:39 -0700

    Add index filter view.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,7 +5,7 @@ class StaticController < ApplicationController
   end
 
   def show
-    if params[:path] == "index"
+    if params[:path] =~ /index/
   	  @paginated_items = Kaminari.paginate_array((1..200).to_a).page(params[:page]).per(20)
     end
     render template: "static/#{params[:path]}"
